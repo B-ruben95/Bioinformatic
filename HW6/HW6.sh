6be1976a08f5790a1a99d72cd7d860d8538e0192
B-ruben95 <martinr.rubend@gmail.com>
2024-10-07 00:04:45 -0400

    Create folder

---
+++ HW6/HW6.sh
@@ -0,0 +1,50 @@
+set -uex
+
+#Variables
+
+NCBI=SRR12432506
+
+FASTQ1=READS/${NCBI}_1.fastq
+FASTQ2=READS/${NCBI}_2.fastq
+
+TRIMMED1=READS/${NCBI}_1.fastq.trimmed.fastq
+TRIMMED2=READS/${NCBI}_2.fastq.trimmed.fastq
+
+
+
+#make a directory for the data
+#mkdir -p READS REPORTS
+
+
+#Download the data
+#fastq-dump --split-files $NCBI -O READS
+echo "The data have been downloaded"
+
+#Quality control
+fastqc -q -o REPORTS $FASTQ1 $FASTQ2
+echo "Quality control done"
+
+#Trimming
+
+fastp -i ${FASTQ1} -I ${FASTQ2} -o ${TRIMMED1} -O ${TRIMMED2} \
+      --detect_adapter_for_pe --cut_tail -w 4
+echo "Trimming done"
+
+#Quality control
+
+fastqc -q -o REPORTS $TRIMMED1 $TRIMMED2
+echo "Quality control done"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
